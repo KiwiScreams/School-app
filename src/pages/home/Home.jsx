19d63ff864de63fca8e7ab8a49bb76deda86e545
KiwiScreams <screamskiwi3@gmail.com>
2024-09-01 11:27:57 +0400

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,50 @@
 import "./Home.css";
 const Home = () => {
+    const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3030/objects")
+      .then((res) => {
+        const dataWithAverageGrade = res.data.map((item) => {
+          if (item.grades && item.grades.length > 0) {
+            const averageGrade =
+              item.grades.reduce((a, b) => a + b, 0) / item.grades.length;
+            return { ...item, averageGrade };
+          } else {
+            return item;
+          }
+        });
+        setData(dataWithAverageGrade);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  function handleDelete(id) {
+    axios.delete("http://localhost:3030/objects/" + id).then((res) => {
+      axios
+        .get("http://localhost:3030/objects")
+        .then((res) => {
+          const dataWithAverageGrade = res.data.map((item) => {
+            if (item.grades && item.grades.length > 0) {
+              const averageGrade =
+                item.grades.reduce((a, b) => a + b, 0) / item.grades.length;
+              return { ...item, averageGrade };
+            } else {
+              return item;
+            }
+          });
+          setData(dataWithAverageGrade);
+          setIsDeleted(true);
+          setTimeout(() => {
+            setIsDeleted(false);
+          }, 2000);
+        })
+        .catch((err) => console.log(err));
+    });
+  }
+
     return ( 
         <></>
      );
