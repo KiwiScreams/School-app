d82c70c604394b2dea32643ec9a46e0efc5475b1
KiwiScreams <screamskiwi3@gmail.com>
2024-09-01 11:35:50 +0400

    update

--- src/pages/update/Update.jsx
+++ src/pages/update/Update.jsx
@@ -1,5 +1,40 @@
 import "./Update.css";
 const Update = () => {
+    const { id } = useParams();
+    const [inputData, setInputData] = useState({
+      id: id,
+      name: "",
+      image: "",
+      color: "",
+    });
+    const navigate = useNavigate();
+    const [isFormValid, setIsFormValid] = useState(false);
+  
+    useEffect(() => {
+      axios
+        .get("http://localhost:3030/objects/" + id)
+        .then((res) => setInputData(res.data))
+        .catch((err) => console.log(err));
+    }, []);
+  
+    useEffect(() => {
+      if (inputData.name && inputData.image && inputData.color) {
+        setIsFormValid(true);
+      } else {
+        setIsFormValid(false);
+      }
+    }, [inputData]);
+    const [isCreated, setIsCreated] = useState(false);
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      setIsCreated(true);
+      axios.put("http://localhost:3030/objects/" + id, inputData).then((res) => {
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      });
+    };
     return ( 
         <></>
      );
