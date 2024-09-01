89dd70f8837e0821c764710f07608d7775d46a7e
KiwiScreams <screamskiwi3@gmail.com>
2024-09-01 14:37:23 +0400

    detail page

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,8 +1,80 @@
 import "./Detail.css";
 const Detail = () => {
-    return ( 
-        <></>
-     );
-}
- 
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+  const [newGrade, setNewGrade] = useState("");
+  const [showInput, setShowInput] = useState(false);
+  const [averageGrade, setAverageGrade] = useState(0);
+  const [newDate, setNewDate] = useState("");
+  useEffect(() => {
+    axios
+      .get("http://localhost:3030/objects/" + id)
+      .then((res) => {
+        setData(res.data);
+        calculateAverageGrade(res.data.grades);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return <><div className="full" style={{ backgroundColor: `${data.color}` }}>
+  <Link to="/" className="create-btn">
+    <i className="fa-solid fa-arrow-left"></i>
+  </Link>
+  <section className="read-section">
+    <div>
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${data.image})`,
+        }}
+      ></div>
+      <div className="read-container">
+        <h1 style={{ color: `${data.color}` }}>{data.name}</h1>
+        <div className="flex">
+          <div>
+            <p className="grade">
+              ქულა: {data.grades && data.grades.join(" | ")}
+            </p>
+            <p>საშუალო ქულა: {averageGrade.toFixed(1)}</p>
+          </div>
+          <button
+            onClick={() => setShowInput(true)}
+            style={{
+              backgroundColor: data.color ? data.color : "black",
+              color: data.color
+                ? isDarkColor(data.color)
+                  ? "white"
+                  : "black"
+                : "black",
+            }}
+          >
+            დააამატე ქულა
+          </button>
+        </div>
+        {showInput && (
+          <div className="show-input">
+            <input
+              type="number"
+              value={newGrade}
+              onChange={(e) => setNewGrade(e.target.value)}
+              placeholder="დაამატე ქულა"
+              style={{ border: `${data.color} 3px solid` }}
+            />
+            <button
+              onClick={handleAddGrade}
+              style={{
+                backgroundColor: data.color ? data.color : "black",
+                color: data.color
+                  
+              }}
+            >
+              შენახვა
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  </section>
+</div></>;
+};
+
 export default Detail;
