683618263fc280ed147c1c385a8e1f00c1e0f6d2
KiwiScreams <screamskiwi3@gmail.com>
2024-09-01 14:40:59 +0400

    average grade

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,4 +1,10 @@
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import "./Detail.css";
+import tinycolor from "tinycolor2";
 const Detail = () => {
   const { id } = useParams();
   const [data, setData] = useState([]);
@@ -15,66 +21,102 @@ const Detail = () => {
       })
       .catch((err) => console.log(err));
   }, []);
-  return <><div className="full" style={{ backgroundColor: `${data.color}` }}>
-  <Link to="/" className="create-btn">
-    <i className="fa-solid fa-arrow-left"></i>
-  </Link>
-  <section className="read-section">
-    <div>
-      <div
-        className="image"
-        style={{
-          backgroundImage: `url(${data.image})`,
-        }}
-      ></div>
-      <div className="read-container">
-        <h1 style={{ color: `${data.color}` }}>{data.name}</h1>
-        <div className="flex">
+
+  const handleAddGrade = () => {
+    const newGradeObject = {
+      grade: Number(newGrade),
+      date: newDate,
+    };
+    axios
+      .patch("http://localhost:3030/objects/" + id, {
+        grades: [...data.grades, newGradeObject],
+      })
+      .then((res) => {
+        const updatedData = {
+          ...data,
+          grades: [...data.grades, newGradeObject],
+        };
+        setData(updatedData);
+        setNewGrade("");
+        setNewDate("");
+        setShowInput(false);
+        calculateAverageGrade(updatedData.grades);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const calculateAverageGrade = (grades) => {
+    setAverageGrade(
+      grades.length === 0
+        ? 0
+        : grades.reduce((a, b) => a + b, 0) / grades.length
+    );
+  };
+  return (
+    <>
+      <div className="full" style={{ backgroundColor: `${data.color}` }}>
+        <Link to="/" className="create-btn">
+          <i className="fa-solid fa-arrow-left"></i>
+        </Link>
+        <section className="read-section">
           <div>
-            <p className="grade">
-              ქულა: {data.grades && data.grades.join(" | ")}
-            </p>
-            <p>საშუალო ქულა: {averageGrade.toFixed(1)}</p>
-          </div>
-          <button
-            onClick={() => setShowInput(true)}
-            style={{
-              backgroundColor: data.color ? data.color : "black",
-              color: data.color
-                ? isDarkColor(data.color)
-                  ? "white"
-                  : "black"
-                : "black",
-            }}
-          >
-            დააამატე ქულა
-          </button>
-        </div>
-        {showInput && (
-          <div className="show-input">
-            <input
-              type="number"
-              value={newGrade}
-              onChange={(e) => setNewGrade(e.target.value)}
-              placeholder="დაამატე ქულა"
-              style={{ border: `${data.color} 3px solid` }}
-            />
-            <button
-              onClick={handleAddGrade}
+            <div
+              className="image"
               style={{
-                backgroundColor: data.color ? data.color : "black",
-                color: data.color
-                  
+                backgroundImage: `url(${data.image})`,
               }}
-            >
-              შენახვა
-            </button>
+            ></div>
+            <div className="read-container">
+              <h1 style={{ color: `${data.color}` }}>{data.name}</h1>
+              <div className="flex">
+                <div>
+                  <p className="grade">
+                    ქულა: {data.grades && data.grades.join(" | ")}
+                  </p>
+                  <p>საშუალო ქულა: {averageGrade.toFixed(1)}</p>
+                </div>
+                <button
+                  onClick={() => setShowInput(true)}
+                  style={{
+                    backgroundColor: data.color ? data.color : "black",
+                    color: data.color
+                     
+                  }}
+                >
+                  დააამატე ქულა
+                </button>
+              </div>
+              {showInput && (
+                <div className="show-input">
+                  <input
+                    type="number"
+                    value={newGrade}
+                    onChange={(e) => setNewGrade(e.target.value)}
+                    placeholder="დაამატე ქულა"
+                    style={{ border: `${data.color} 3px solid` }}
+                  />
+                  <button
+                    onClick={handleAddGrade}
+                    style={{
+                      backgroundColor: data.color ? data.color : "black",
+                      color: data.color
+                        ? isDarkColor(data.color)
+                          ? "white"
+                          : "black"
+                        : "black",
+                    }}
+                    disabled={newGrade === ""}
+                  >
+                    შენახვა
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
-        )}
+        </section>
       </div>
-    </div>
-  </section>
-</div></>;
+    </>
+  );
 };
 
 export default Detail;
