ff2cc036d0a31f5156d7b162d3561c80a0947dce
KiwiScreams <screamskiwi3@gmail.com>
2024-09-01 11:23:15 +0400

    create

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,8 +1,34 @@
 import "./Create.css";
 const Create = () => {
-    return ( 
-        <></>
-     );
-}
- 
+  const [inputData, setInputData] = useState({
+    name: "",
+    image: "",
+    color: "",
+    grades: [],
+  });
+  const navigate = useNavigate();
+  const { name, image, color } = inputData;
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    if (name && image) {
+      setIsFormValid(true);
+    } else {
+      setIsFormValid(false);
+    }
+  }, [name, image, color]);
+  const [isCreated, setIsCreated] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post("http://localhost:3030/objects", inputData).then((res) => {
+      setIsCreated(true);
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    });
+  };
+  return <></>;
+};
+
 export default Create;
